5979b9471d5185418e5cb93f6f41b69ad474b31c
sinebonisimo <sinebonisimo@sinem-pc>
2013-07-01 10:58:30 +0300

    2.ekrana yazdırma

--- src/gitapplications/GitApplications.java
+++ src/gitapplications/GitApplications.java
@@ -19,7 +19,7 @@ public class GitApplications {
         // TODO code application logic here
         String s;
         s="sdbjdsfv";
-        System.out.println("first!!");
+        System.out.println("second!!");
         
     }
 }
