1a19305cef0b06af6e4dfa3e64871b60d3148bc0
sinebonisimo <sinebonisimo@sinem-pc>
2013-07-01 10:06:46 +0300

    ekrana yazdırma

---
+++ src/gitapplications/GitApplications.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitapplications;
+
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+/**
+ *
+ * @author sinebonisimo
+ */
+public class GitApplications {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String s;
+        s="sdbjdsfv";
+        System.out.println("first!!");
+        
+    }
+}
